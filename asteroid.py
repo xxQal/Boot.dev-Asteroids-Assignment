d2603bc70d9c6d1ab5465eddac0e2ddc6506c06c
Qal <xqalbring191@gmail.com>
2026-04-25 17:40:07 -0400

    Main updated, Asteroid addition of split & spawning 2 asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +14,18 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            new_direction = random.uniform(20, 50)
+            split1 = self.velocity.rotate(new_direction)
+            split2 = self.velocity.rotate(-new_direction)
+            new_radius = (self.radius - ASTEROID_MIN_RADIUS)
+            a1 = Asteroid(self.position.x, self.position.y, new_radius)
+            a1.velocity = (split1 * 1.2)
+            a2 = Asteroid(self.position.x, self.position.y, new_radius)
+            a2.velocity = (split2 * 1.2)
--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
                     if shot.collides_with(asteroid):
                         log_event("asteroid_shot")
                         shot.kill()
-                        asteroid.kill()
+                        asteroid.split()
                 if player.collides_with(asteroid):
                     log_event("player_hit")
                     print("Game over!")
